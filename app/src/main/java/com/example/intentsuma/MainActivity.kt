7ef797ebefc1a385fac1adbcef0bd26236ee7ca9
arh91 <alvaro_rh_991@hotmail.com>
2023-03-16 03:20:54 +0100

    Envío dos numeros del MainActivity al SecondActivity y éste devuelve el resultado de la suma de ambos al MainActivity.

--- app/src/main/java/com/example/intentsuma/MainActivity.kt
+++ app/src/main/java/com/example/intentsuma/MainActivity.kt
@@ -1,11 +1,75 @@
 package com.example.intentsuma
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var resultadoRecibido:Boolean = false
+    val RESULTADO = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var hacerFoto = findViewById<Button>(R.id.foto_Btn)
+        var enviarDatos = findViewById<Button>(R.id.envioDatos_Btn)
+        var siguiente = findViewById<Button>(R.id.siguiente_Btn)
+
+        hacerFoto.setOnClickListener(){
+
+        }
+
+        enviarDatos.setOnClickListener(){
+            val second = SecondActivity()
+            val intent = getIntent()
+
+            var numeroUno = findViewById<TextView>(R.id.numeroUno)
+            var numeroDos = findViewById<TextView>(R.id.numeroDos)
+
+
+            intent.putExtra("PrimerNumero", numeroUno.text) //Enviamos al SecondActivity la cadena del TextView "numeroUno" asignándole a dicha cadena el id "PrimerNumero"
+            intent.putExtra("SegundoNumero", numeroDos.text) //Enviamos al SecondActivity la cadena del TextView "numeroDos" asignándole a dicha cadena el id "SegundoNumero"
+            setResult(Activity.RESULT_OK, intent);
+
+            second.datosRecibidos() //Avisamos a SecondActivity de que ya le hemos enviado los datos
+        }
+
+        siguiente.setOnClickListener(){
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+        if(resultadoRecibido == true){
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivityForResult(intent, RESULTADO)
+        }
+
+    }
+
+    //Este método recibe del SecondActivity una cadena con id "Resultado" y la muestra en el TextView "Resultado"
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val Resultado = findViewById<TextView>(R.id.Resultado)
+        if(resultCode != Activity.RESULT_OK) return
+        when(requestCode) {
+            RESULTADO -> {
+                if (data != null) {
+                    Resultado.text = data.getStringExtra("Resultado")
+                }; }
+            // Other result codes
+            else -> {}
+        }
+    }
+
+    /*Método que será llamado desde el SecondActivity cuando el usuario pulse el botón Enviar Resultado
+    y se haya pasado el resultado del SecondActivity al MainActivity*/
+    fun recibirResultado(){
+        resultadoRecibido = true
     }
 }
--- app/src/main/java/com/example/intentsuma/SecondActivity.kt
+++ app/src/main/java/com/example/intentsuma/SecondActivity.kt
@@ -1,11 +1,113 @@
 package com.example.intentsuma
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class SecondActivity : AppCompatActivity() {
+
+    val NUMERO_UNO = 1
+    val NUMERO_DOS = 2
+
+    var datosRecibidos:Boolean = false
+    var resultadoObtenido:Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        //var recibido:Boolean = false
+
+        var numeroUno = findViewById<TextView>(R.id.numeroUno)
+        var numeroDos = findViewById<TextView>(R.id.numeroDos)
+        val resultado = findViewById<TextView>(R.id.resultadoSuma)
+
+
+        var anterior = findViewById<Button>(R.id.anterior_Btn)
+        var enviarResultado = findViewById<Button>(R.id.enviarResultado_Btn)
+
+        anterior.setOnClickListener(){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        enviarResultado.setOnClickListener(){
+            var main = MainActivity()
+            if(resultadoObtenido == true) {
+                val intent = getIntent()
+                intent.putExtra("Resultado", resultado.text) //Enviamos al MainActivity la cadena del TextView "resultado" asignándole a dicha cadena el id "Resultado"
+                setResult(Activity.RESULT_OK, intent);
+
+                main.recibirResultado() //Avisamos a MainActivity de que ya le hemos enviado el Resultado de la suma
+            }else{
+                Toast.makeText(this@SecondActivity, "Lo sentimos, no existe un resultado para enviar.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        if(datosRecibidos == true){
+            recogerDatos()
+
+            var numeroUnoInt: Int = numeroUno.text.toString().toInt()
+            var numeroDosInt: Int = numeroDos.text.toString().toInt()
+
+            val resultadoInt: Int = suma(numeroUnoInt, numeroDosInt)
+            val resultadoString = resultadoInt.toString()
+
+            resultado.text = resultadoString
+            resultadoObtenido = true
+        }
+    }
+
+
+    /*Este método recibe del MainActivity una cadena de id "PrimerNumero" y la muestra en el TextView "numero1"
+    y recibe otra cadena con id "SegundoNumero" y la muestra en el TextView "numero2"*/
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val numeroUno = findViewById<TextView>(R.id.numero1)
+        val numeroDos = findViewById<TextView>(R.id.numero2)
+        if(resultCode != Activity.RESULT_OK) return
+        when(requestCode) {
+            NUMERO_UNO -> {
+                if (data != null) {
+                    numeroUno.text = data.getStringExtra("PrimerNumero")
+                }; }
+            // Other result codes
+            else -> {}
+        }
+        when(requestCode) {
+            NUMERO_DOS -> {
+                if (data != null) {
+                    numeroDos.text = data.getStringExtra("SegundoNumero")
+                }; }
+            // Other result codes
+            else -> {}
+        }
+    }
+
+
+    //Método que recibe dos numeros y devuelve la suma de ambos
+    fun suma(numeroUno: Int, numeroDos: Int): Int {
+        val suma: Int = numeroUno + numeroDos
+        return suma
+    }
+
+
+
+    fun recogerDatos(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivityForResult(intent, NUMERO_UNO)
+        startActivityForResult(intent, NUMERO_DOS)
+    }
+
+
+    /*Método que será llamado desde el MainActivity una vez que el usuario pulse el botón Enviar Datos y se hayan pasado los
+    dos numeros desde el MainActivity al SecondActivity*/
+    fun datosRecibidos(){
+        datosRecibidos = true
     }
 }
